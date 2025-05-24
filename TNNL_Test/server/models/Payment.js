802be0ab07135f4b435e929f53b538bfc6e63566
michelleduong03 <miichelleduong@gmail.com>
2025-05-24 12:16:44 -0700

    new model to save payment data

--- TNNL_Test/server/models/Payment.js
+++ TNNL_Test/server/models/Payment.js
@@ -3,11 +3,24 @@ const mongoose = require('mongoose');
 const PaymentSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   provider: String,
-  purchaseAmount: Number,
-  installments: Number,
-  firstDueDate: Date,
-  description: String,
-  createdAt: { type: Date, default: Date.now }
+  subject: String,
+  date: String,
+  merchantName: String,
+  merchantOrder: String,
+  klarnaOrderId: String,
+  totalAmount: String,
+  installmentAmount: String,
+  isFirstPayment: Boolean,
+  paymentPlan: String,
+  orderDate: String,
+  cardUsed: String,
+  discount: String,
+  status: String,
+  nextPaymentDate: String,
+  nextPaymentAmount: String,
+  items: [String],
+  snippet: String,
+  userEmail: String,
 });
 
 module.exports = mongoose.model('Payment', PaymentSchema);
