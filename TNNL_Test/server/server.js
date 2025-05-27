d8cda7132f3cd21cf37a9d4553a5daa8f4035d41
michelleduong03 <miichelleduong@gmail.com>
2025-05-27 11:24:19 -0700

    added some changes

--- TNNL_Test/server/server.js
+++ TNNL_Test/server/server.js
@@ -16,6 +16,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/payments', paymentRoutes);
 app.use('/api/gmail', gmailRoutes);
