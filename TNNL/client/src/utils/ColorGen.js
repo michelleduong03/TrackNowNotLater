408d6fa92411af363602aaae522bb0200f0e960d
michelleduong03 <miichelleduong@gmail.com>
2025-06-17 12:15:42 -0700

    Delete TNNL folder

--- TNNL/client/src/utils/ColorGen.js
+++
@@ -1,9 +0,0 @@
-export function generatePastelColors(numColors) {
-  const colors = [];
-  for (let i = 0; i < numColors; i++) {
-    const hue = Math.floor((360 / numColors) * i);
-    colors.push(`hsl(${hue}, 50%, 80%)`); 
-  }
-  
-  return colors;
-}
